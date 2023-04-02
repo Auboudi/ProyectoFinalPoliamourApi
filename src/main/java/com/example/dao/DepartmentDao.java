d68cfd0435b6cce7e22ce2a559dfee768a04de2b
criscgv7 <cris.cgv7@gmail.com>
2023-04-02 18:17:01 +0200

    ServiceDone

--- src/main/java/com/example/dao/DepartmentDao.java
+++ src/main/java/com/example/dao/DepartmentDao.java
@@ -4,11 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entities.Department;
 
-<<<<<<< HEAD
-public interface DepartmentDao extends JpaRepository<Department, Long>{
-=======
 public interface DepartmentDao extends JpaRepository<Department, Long>   {
     
->>>>>>> origin/ramaMara
     
 }
---
+++ src/main/java/com/example/services/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.example.dao.UserDao;
+import com.example.entities.User;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return userDao.findAll(pageable);
+    }
+
+    @Override
+    public List<User> findAll(Sort sort) {
+return userDao.findAll(sort);    }
+
+    @Override
+    public User findbyId(long id) {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userDao.save(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        userDao.delete(user);
+    }
+
+    @Override
+    public void deletebyId(long id) {
+        userDao.deleteById(id);
+    }
+
+}
