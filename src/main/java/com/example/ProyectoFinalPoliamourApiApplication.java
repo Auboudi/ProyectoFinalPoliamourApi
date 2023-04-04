04644b365905d63c06b4ed77bd63fed133a2bca2
=ddelvill <dayanasdelvillar@gmail.com>
2023-04-04 17:17:45 +0200

    Commit pre merge Mara

--- src/main/java/com/example/ProyectoFinalPoliamourApiApplication.java
+++ src/main/java/com/example/ProyectoFinalPoliamourApiApplication.java
@@ -1,10 +1,38 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.entities.Department;
+import com.example.services.DepartmentService;
+import com.example.services.HobbieService;
+import com.example.services.PhoneService;
+import com.example.services.UserService;
+import com.example.services.YardService;
+
 @SpringBootApplication
-public class ProyectoFinalPoliamourApiApplication {
+public class ProyectoFinalPoliamourApiApplication  implements CommandLineRunner{
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@Autowired
+	private PhoneService phoneService;
+
+	@Autowired
+	private HobbieService hobbieService;
+
+	@Autowired
+	private YardService yardService;
+
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalPoliamourApiApplication.class, args);
@@ -14,4 +42,22 @@ public class ProyectoFinalPoliamourApiApplication {
 	
 
 }
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		departmentService.save(Department.builder()
+			.name("Informatica")
+			.build());
+		
+		
+		
+
+		departmentService.save(Department.builder()
+			.name("RRHH")
+			.build());
+
+		
+			
+	}
 }
