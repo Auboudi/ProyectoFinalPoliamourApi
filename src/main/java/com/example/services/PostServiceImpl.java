450704b3b45e719311261cc4e366bdb96fd32b80
belco <bcollado91@hotmail.com>
2023-04-05 17:00:58 +0200

    post service creado

--- src/main/java/com/example/services/PostServiceImpl.java
+++ src/main/java/com/example/services/PostServiceImpl.java
@@ -1,8 +1,38 @@
 package com.example.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.entities.Post;
 
 @Service
 public class PostServiceImpl implements PostService {
+
+    
+
+    @Override
+    public List<Post> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Post findbyId(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findbyId'");
+    }
+
+    @Override
+    public Post save(Post post) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void delete(Post post) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
     
 }
