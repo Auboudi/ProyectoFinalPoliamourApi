c83348b67b654bb02483ae85c7d3e16a817de36c
belco <bcollado91@hotmail.com>
2023-04-05 09:35:19 +0200

    postdao empezado

--- src/main/java/com/example/services/PostService.java
+++ src/main/java/com/example/services/PostService.java
@@ -1,5 +1,13 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.entities.Post;
+
 public interface PostService {
-    
+
+    public List<Post> findAll();
+    public Post findbyId (long id); 
+    public Post save (Post post);
+    public void delete (Post post); 
 }
