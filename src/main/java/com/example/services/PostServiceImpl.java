abbaa572f0d2228574d6d4fb178a8bd9e42ef034
=ddelvill <dayanasdelvillar@gmail.com>
2023-04-06 20:37:24 +0200

    Post Dao Service

--- src/main/java/com/example/services/PostServiceImpl.java
+++ src/main/java/com/example/services/PostServiceImpl.java
@@ -2,37 +2,56 @@ package com.example.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.dao.PostDao;
 import com.example.entities.Post;
 
 @Service
 public class PostServiceImpl implements PostService {
 
-    
+    @Autowired
+    private PostDao postDao;
 
     @Override
-    public List<Post> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    public List<Post> findAll(Sort sort) {
+        return postDao.findAll(sort);
+    }
+
+    @Override
+    public Page<Post> findAll(Pageable pageable) {
+        return postDao.findAll(pageable);
     }
 
     @Override
     public Post findbyId(long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findbyId'");
+        return postDao.findById(id);
     }
 
     @Override
+    @Transactional
     public Post save(Post post) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        return postDao.save(post);
     }
 
     @Override
+    @Transactional
     public void delete(Post post) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        postDao.delete(post);
     }
+
+    @Override
+    public List<Post> findByUserId(long id) {
+        return postDao.findByUserId(id);
+    }
+
+    
+
+
     
 }
